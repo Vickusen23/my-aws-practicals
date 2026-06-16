5c5f5dc96d88abc1cfa40b4baa83a80e7d70a7f1
Victor Pius Usen <vickusen1@gmail.com>
2026-06-16 16:44:24 +0100

    Delete Architecture-Diagram/Screenshots directory

--- Architecture-Diagram/Screenshots/User-Data/apache-userdata.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-yum update -y
-yum install -y httpd
-systemctl start httpd
-systemctl enable httpd
-
-cat <<EOF > /var/www/html/index.html
-Hello world! Welcome to Vick's SAA Practical
-EOF
